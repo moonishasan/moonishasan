ef07bddbf0fa5c76f5afe04e01ddf76be2a2f23f
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-08-27 19:57:07 +0530

    Time: 311 ms (28.90%) | Memory: 203.1 MB (33.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
+        unordered_set<int> s(nums.begin(), nums.end()); // to remove any duplicates
         vector<int> nums2(s.begin(), s.end());
-        sort(nums2.begin(), nums2.end());
+        sort(nums2.begin(), nums2.end());  // sort in ascending order
+        // now check LCS in nums and nums2 -> LIS in nums
 
         int n = nums.size();
         int m = nums2.size();
+
         vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
-        // LCS => LIS
-        for(int i = 1; i < n+1 ; i++){
+        for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m+1; j++){
                 if(nums[i-1] == nums2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
