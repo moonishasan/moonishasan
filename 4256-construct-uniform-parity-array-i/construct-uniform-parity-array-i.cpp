931cde2d4c2ee86085e7324a99eea530263b83de
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-02 13:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        vector<int> nums2;
+        int n = nums1.size();
+        int count_of_even = 0;
+        int count_of_odd = 0;
+        for(int num : nums1){
+            if(num % 2 == 0){
+                count_of_even++;
+            }
+            count_of_odd++;
+        }
+        if(count_of_even == n){
+            return true;
+        }
+        else if(count_of_odd == n){
+            return true;
+        }
+        return true;
+    }
+};
