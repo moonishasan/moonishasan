c79dd61c2b9071b65dfac04a8f76127f046522f9
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-04 18:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (79.96%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
         int n = nums.size();
-        vector<float> averages;
         sort(nums.begin(), nums.end());
         float mini = INT_MAX;
         for(int i = 0; i < n/2; i++){
