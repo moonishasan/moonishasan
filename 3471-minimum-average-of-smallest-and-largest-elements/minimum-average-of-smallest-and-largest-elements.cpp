04915e01007966b6f2d9e82642955f7df54dfceb
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-04 18:17:17 +0530

    Time: 4 ms (5.35%) | Memory: 25.5 MB (79.96%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -4,13 +4,10 @@ public:
         int n = nums.size();
         vector<float> averages;
         sort(nums.begin(), nums.end());
+        float mini = INT_MAX;
         for(int i = 0; i < n/2; i++){
             float val = (nums[i] + nums[n-i-1]) / 2.0;
-            averages.push_back(val);
-        }
-        float mini = INT_MAX;
-        for(auto n : averages){
-            mini = min(mini, n);
+            mini = min(mini, val);
         }
         return mini;
     }
