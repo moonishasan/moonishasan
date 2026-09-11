91a73f7253dc5af8f9b60daaeb717c13a0d236e5
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-11 16:19:02 +0530

    Time: 7 ms (11.19%) | Memory: 33.4 MB (16.31%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.cpp
+++ 4354-unique-middle-element/unique-middle-element.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
         unordered_map<int, int> freq;
+        int n = nums.size();
+        int mid = n/2;
         for(int n : nums){
             freq[n]++;
         }
-        if(freq[nums[nums.size()/2]] == 1){
+        if(freq[nums[mid]] == 1){
             return true;
         }
         return false;
