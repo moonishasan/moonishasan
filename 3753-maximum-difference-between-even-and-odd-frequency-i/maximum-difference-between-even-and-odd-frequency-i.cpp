ceb2208e7046e279d10b2fd2cbff3c536b8a81f0
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-13 13:36:06 +0530

    Time: 2 ms (42.15%) | Memory: 10 MB (27.16%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -8,11 +8,11 @@ public:
         int maxi = INT_MIN;  // odd
         int mini = INT_MAX;  // even
         for(auto it : freq){
-            if(it.second % 2 != 0){
+            if(it.second % 2 != 0){  // max odd freq
                 maxi = max(maxi, it.second);
                 continue;
             }
-            if(it.second % 2 == 0){
+            if(it.second % 2 == 0){   // min even freq
                 mini = min(mini, it.second);
                 continue;
             }
