944ee16d76f500f3032a6e4f7361506fd8a78833
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-14 01:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (95.36%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.cpp
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int generateKey(int num1, int num2, int num3) {
         int ans = 0;
+        int place = 1;
         for(int i = 0; i < 4; i++){
             int dig1 = num1 % 10;
             int dig2 = num2 % 10;
             int dig3 = num3 % 10;
-            ans += min(min(dig1, dig2), dig3) * pow(10, i);
+            ans += min(min(dig1, dig2), dig3) * place;
             num1 /= 10;
             num2 /= 10;
             num3 /= 10;
+            place *= 10;
         }
         return ans;
-
     }
 };
