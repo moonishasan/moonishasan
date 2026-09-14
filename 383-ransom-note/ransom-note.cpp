e3e6050202970691fadf5d1085e99d06d86ba683
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-14 16:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (49.65%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> freq;
+        int freq[26] = {0};
         for(char c : magazine){
-            freq[c]++;
+            freq[c-'a']++;
         }
         for(int i = 0; i < ransomNote.size(); i++){
             // if the char that exist in ransomNote does not exist in magazine
-            if(freq[ransomNote[i]] == 0){
+            if(freq[ransomNote[i]-'a'] == 0){
                 return false;
             }
             // else if that character exists 
-            else if(freq.count(ransomNote[i])){
-                freq[ransomNote[i]]--;
+            else if(freq[ransomNote[i]-'a'] > 0){
+                freq[ransomNote[i]-'a']--;
             }
         }
         return true;
