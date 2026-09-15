2bb4c67b52ef4f87abe58677b5ebc62f8349eb3a
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-15 23:11:21 +0530

    Time: 23 ms (23.50%) | Memory: 160.5 MB (16.97%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        int level_no = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i = 0; i < size; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                level.push_back(curr->val);
+                if(curr->left != nullptr){
+                    q.push(curr->left);
+                }
+                if(curr->right != nullptr){
+                    q.push(curr->right);
+                }
+            }
+            if(level_no % 2 == 0){  // level is even
+                for(int i = 0; i < level.size(); i++){
+                    if(level[i] % 2 == 0){
+                        return false;
+                    }
+                }
+                for(int i = 0; i < level.size()-1; i++){
+                    if(level[i] >= level[i+1]){
+                        return false;
+                    }
+                }
+            }
+            else if(level_no % 2 != 0){     // level is odd
+                for(int i = 0; i < level.size(); i++){
+                    if(level[i] % 2 != 0){
+                        return false;
+                    }
+                }
+                for(int i = 0; i < level.size()-1; i++){
+                    if(level[i] <= level[i+1]){
+                        return false;
+                    }
+                }
+            }
+            level_no++;
+        }
+        return true;
+    }
+};
