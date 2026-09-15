af9172c7d995c41bfada6a6ac3a3d230a152d4b1
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-15 17:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (91.36%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
-        int ans = 0, start = 0;
+        // start = first index from where we are allowed to select our next palindrome. This prevents overlapping substrings.
+        // ans = number of palindromic substrings we have selected
+        int ans = 0;
+        int start = 0;
+        // Lambda function to check whether s[l...r] is a palindrome
         auto check = [&](int l, int r) {
             while (l < r) {
                 if (s[l++] != s[r--]) {
@@ -11,14 +15,14 @@ public:
             }
             return true;
         };
-        for (int r = k - 1; r < n; r++) {
+        for(int r = k - 1; r < n; r++){
             int l = r - k + 1;
             if (l >= start && check(l, r)) {
                 ans++;
                 start = r + 1;
                 continue;
             }
-
+            // If length k didn't work, check length k+1.
             l = r - k;
             if (l >= start && check(l, r)) {
                 ans++;
