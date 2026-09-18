e09be98c9f4785b1bfae7b95ee7bad133fb94e17
Moonis Hasan <146074856+moonishasan@users.noreply.github.com>
2026-09-19 00:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (43.69%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -4,9 +4,6 @@ public:
         if(n < 0){
             return false;
         } 
-        if(n == 1){
-            return true;
-        }
         while(n >= 1){
             if(n % 2 == 0){
                 n /= 2;
